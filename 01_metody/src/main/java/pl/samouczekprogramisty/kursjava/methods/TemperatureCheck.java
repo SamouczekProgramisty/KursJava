ca8d618e40d2896a882646c29bc4b296406b5f9e
Marcin Pietraszek <m.pietraszek@gmail.com>
2017-07-17 22:45:11 +0200

    Simplified version of temperature checking method.

--- 01_metody/src/main/java/pl/samouczekprogramisty/kursjava/methods/TemperatureCheck.java
+++ 01_metody/src/main/java/pl/samouczekprogramisty/kursjava/methods/TemperatureCheck.java
@@ -2,32 +2,23 @@ package pl.samouczekprogramisty.kursjava.methods;
 
 public class TemperatureCheck {
     public static void main(String... args) {
-        validateArguments(args);
+        int positiveTemperature = 10;
+        int negativeTemperature = -10;
 
-        TemperatureCheck temperatureCheck = new TemperatureCheck();
-        if (temperatureCheck.isTemperaturePositive(Double.parseDouble(args[0]))) {
-            System.out.println("Wprowadzona temperatura jest dodatnia.");
+        isTemperaturePositive(positiveTemperature);
+        isTemperaturePositive(negativeTemperature);
+    }
+
+    private static boolean isTemperaturePositive(double temperature) {
+        boolean isPositive = temperature > 0;
+
+        if (isPositive) {
+            System.out.println("Temperatura " + temperature + " jest dodatnia.");
         }
         else {
-            System.out.println("Wprowadzona temperatura nie jest dodatnia.");
+            System.out.println("Temperatura " + temperature + " nie jest dodatnia.");
         }
-    }
 
-    private static void validateArguments(String[] args) {
-        if (args.length != 1) {
-            System.err.println("Prosze podaj temperature jako jedyny parametr.");
-            System.exit(1);
-        }
-        try {
-            Double.parseDouble(args[0]);
-        }
-        catch (NumberFormatException exception) {
-            System.err.println("Podana temperatura nie jest poprawna liczbą zmienna przecinkową!");
-            System.exit(2);
-        }
-    }
-
-    private boolean isTemperaturePositive(double temperature) {
-        return temperature > 0;
+        return isPositive;
     }
 }
