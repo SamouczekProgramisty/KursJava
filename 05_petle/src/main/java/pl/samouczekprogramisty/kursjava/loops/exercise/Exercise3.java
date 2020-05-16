2a5b0c7dcb257e39798d26e026caa31b73c6dc98
Marcin Pietraszek <m.pietraszek@gmail.com>
2020-05-16 20:55:19 +0200

    Fix even/odd error pointed out by KonradZ

--- 05_petle/src/main/java/pl/samouczekprogramisty/kursjava/loops/exercise/Exercise3.java
+++ 05_petle/src/main/java/pl/samouczekprogramisty/kursjava/loops/exercise/Exercise3.java
@@ -2,8 +2,10 @@ package pl.samouczekprogramisty.kursjava.loops.exercise;
 
 public class Exercise3 {
     public static void main(String[] args) {
-        for(int evenNumber = -10; evenNumber < 41; evenNumber += 2) {
-            System.out.println(evenNumber);
+        for(int number = -10; number <= 40; number++) {
+            if (number % 2 != 0) {
+                System.out.println(number);
+            }
         }
     }
 }
