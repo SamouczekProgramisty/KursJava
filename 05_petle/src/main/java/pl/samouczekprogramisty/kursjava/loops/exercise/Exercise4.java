30d5b6d46982d023672ec75d506e7b1db1bdac9d
Marcin Pietraszek <m.pietraszek@gmail.com>
2022-08-17 22:28:01 +0200

    Print odd numbers

--- 05_petle/src/main/java/pl/samouczekprogramisty/kursjava/loops/exercise/Exercise4.java
+++ 05_petle/src/main/java/pl/samouczekprogramisty/kursjava/loops/exercise/Exercise4.java
@@ -2,10 +2,10 @@ package pl.samouczekprogramisty.kursjava.loops.exercise;
 
 public class Exercise4 {
     public static void main(String[] args) {
-        int evenNumber = -10;
-        while(evenNumber < 41) {
-            System.out.println(evenNumber);
-            evenNumber += 2;
+        int oddNumber = -9;
+        while(oddNumber < 41) {
+            System.out.println(oddNumber);
+            oddNumber += 2;
         }
     }
 }
